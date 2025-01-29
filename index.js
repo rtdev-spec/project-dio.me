27a17125f07552d8d7566977b321e2a1344422f1
Renato Tavares <rtzikadeveloper@gmail.com>
2025-01-29 01:02:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-// Importando o Express
+
 const express = require('express');
 const app = express();
 
-// Definindo a porta
+
 const PORT = process.env.PORT || 3000;
 
-// Rota principal
+
 app.get('/', (req, res) => {
     res.json({
-        nome: "Seu Nome", // Substitua pelo seu nome
-        github: "https://github.com/seu-usuario", // Seu perfil no GitHub
-        linkedin: "https://www.linkedin.com/in/seu-perfil", // Opcional
+        nome: "Renato Tavares", // 
+        github: "https://github.com/rtdev-spec", // 
+        linkedin: "https://www.linkedin.com/in/renato-tavares-marques-75146020b", // 
         mensagem: "Olá! Bem-vindo à minha API de apresentação no GitHub!"
     });
 });
 
-// Iniciando o servidor
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
